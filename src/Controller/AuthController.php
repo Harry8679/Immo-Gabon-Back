f91b9776fbbf7d8fde5957d4ec44715612614998
Harry7986 <emarh.harry.code@gmail.com>
2025-10-23 19:41:16 +0200

    Auth Controller 2

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,17 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class AuthController extends AbstractController
 {
-    #[Route('/auth', name: 'app_auth')]
-    public function index(): Response
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): Response
     {
-        return $this->render('auth/index.html.twig', [
-            'controller_name' => 'AuthController',
-        ]);
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setFirstName($data['firstName']);
+        $user->setLastName($data['lastName']);
+        $user->setPhoneNumber($data['phoneNumber']);
+
+        $hashedPassword = $userPasswordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json(['message' => 'Votre compte a bien été créé !']);
     }
 }
