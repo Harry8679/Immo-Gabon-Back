a9f4793ade662719070ccd9828baa970f849475c
Harry7986 <emarh.harry.code@gmail.com>
2025-10-24 00:54:10 +0200

    JWTCreated Listener 2

---
+++ src/EventListener/JWTCreatedSuccessListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class JWTCreatedSuccessListener
+{
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        /** @var User $user */
+        $user = $event->getUser();
+
+        if (!$user) {
+            return;
+        }
+
+        // Ajouter les infos de l'utilisateur à la réponse
+        $data['user'] = [
+            'id' => $user->getId(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+            'email' => $user->getEmail(),
+            'phoneNumber' => $user->getPhoneNumber(),
+            'roles' => $user->getRoles()
+        ];
+
+        $event->setData($data);
+    }
+}
